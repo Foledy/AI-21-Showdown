7550bd7f0ce06e3c7bca3a6e4579fb9cdf2cc5ee
Foled <alesha05sb@gmail.com>
2024-03-17 07:10:56 +0300

    fix(ScriptableObject): Sprite has been replaced by GameObject

--- Assets/Scripts/BlackJack/GameCard.cs
+++ Assets/Scripts/BlackJack/GameCard.cs
@@ -6,11 +6,11 @@ namespace BlackJack
     [CreateAssetMenu(fileName = "GameCard", menuName = "BlackJack/Create Game Card", order = 0)]
     public class GameCard : ScriptableObject, IGameCard
     {
-        [SerializeField] private Sprite _image;
+        [SerializeField] private GameObject _prefab;
         [SerializeField] private int _value;
 
         public int GetValue() => _value;
 
-        public Sprite GetSprite() => _image;
+        public GameObject GetPrefab() => _prefab;
     }
 }
