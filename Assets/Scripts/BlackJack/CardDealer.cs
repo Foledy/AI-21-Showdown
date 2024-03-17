54671791e229e4890c0a35b7772f92978aeefab7
Foled <alesha05sb@gmail.com>
2024-03-17 08:35:01 +0300

    feat: Added BlackJack settings

--- Assets/Scripts/BlackJack/CardDealer.cs
+++ Assets/Scripts/BlackJack/CardDealer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using BlackJack.Interfaces;
 using BlackJack.ScriptableObjects;
 using BlackJack.Types;
---
+++ Assets/Scripts/BlackJack/ScriptableObjects/BlackJackSettings.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace BlackJack.ScriptableObjects
+{
+    [CreateAssetMenu(fileName = "BlackJack Settings", menuName = "BlackJack/Create new BlackJack Settings", order = 0)]
+    public class BlackJackSettings : ScriptableObject
+    {
+        public int PointsToWin;
+        [Tooltip("Place '-1' for unlimited time")]
+        public float TimePerMove;
+    }
+}
