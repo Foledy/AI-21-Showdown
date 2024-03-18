3b6d99df047b32e1bccf71690b65ddfc12715374
Yurii Kartsev <tinazov1999yura@gmaol.com>
2024-03-18 15:55:47 +0100

    'PlayarControler'

--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -1,44 +1,30 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerControler : MonoBehaviour
 {
     //переменная для скорость движения игрока
     [SerializeField] private float _speed;
-    //
+    // переменные для передвижения игрока
+    private float hInput;
+    private float vInput;
     private Rigidbody _rb;
 
     private void Start()
     {
+        hInput = Input.GetAxis("Horizontal");
+        
         _rb = GetComponent<Rigidbody>();
     }
 
     //метод в котором будет происходить движения игрока
     private void PlayerMovement()
     {
-        //движение вперед, назад, вправо, влево
-        if(Input.GetKey(KeyCode.W))
-        {
-            _rb.MovePosition(transform.position + transform.forward * _speed * Time.fixedDeltaTime);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            _rb.MovePosition(transform.position + transform.forward * -_speed * Time.fixedDeltaTime);
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            _rb.MovePosition(transform.position + transform.right * _speed * Time.fixedDeltaTime);
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            _rb.MovePosition(transform.position + transform.right * -_speed * Time.fixedDeltaTime);
-        }
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
 
-        //движение по диагонали 
-      
-
-
+        Vector3 movement = new Vector3(hInput, 0f, vInput);
+        _rb.velocity = movement * _speed * Time.deltaTime;
     }
 
     private void FixedUpdate()
