684e44a90c1638cce5116bc391e7b61ec23ba9ba
Yurii Kartsev <tinazov1999yura@gmaol.com>
2024-03-19 09:55:02 +0100

    'PlayarAnimation'

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,25 @@
 
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class PlayerControler : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] Animator animator;
+    [SerializeField] private Animator _animator;
     //переменная для скорость движения игрока
     [SerializeField] private float _speedPlayarMin = 0;
     [SerializeField] private float _speedPlayarMax = 5;
     // переменные для передвижения игрока
-    private float hInput;
-    private float vInput;
     private Rigidbody _rb;
 
     private void Start()
     {
-        hInput = Input.GetAxis("Horizontal");
-        
+        _animator = GetComponent<Animator>();
         _rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovements();
+        AnimationOff();
     }
 
     //метод для разгона игрока
@@ -31,16 +33,14 @@ public class PlayerControler : MonoBehaviour
     }
 
     //метод в котором будет происходить движения игрока
-    private void PlayerMovement()
+    private void PlayerMovements()
     {
-        hInput = Input.GetAxis("Horizontal");
-      //  vInput = Input.GetAxis("Vertical");
         if(Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.W))
         {
             _rb.MovePosition(transform.position + transform.forward * _speedPlayarMin * Time.fixedDeltaTime);
             if(_speedPlayarMin >= 0.1f)
             {
-                animator.SetBool("Walcing", true);
+                _animator.SetBool("Walcing", true);
             }
             
             AccelerationPlayar(0.03f);
@@ -49,55 +49,46 @@ public class PlayerControler : MonoBehaviour
         if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.S))
         {
             _rb.MovePosition(transform.position + transform.forward * -_speedPlayarMin * Time.fixedDeltaTime);
-            animator.SetBool("WalcingBack", true);
+            _animator.SetBool("WalcingBack", true);
             AccelerationPlayar(0.03f);
         }
 
         if (((Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))))
         {
             _rb.MovePosition(transform.position +(-transform.right + transform.forward) * _speedPlayarMin * Time.fixedDeltaTime);
-            animator.SetBool("LeftForwardDiadonaleStrafeWalking", true);
-            animator.SetBool("Walcing", false);
+            _animator.SetBool("LeftForwardDiadonaleStrafeWalking", true);
+            _animator.SetBool("Walcing", false);
             AccelerationPlayar(0f);
         }
 
         if (((Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W)) || (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))))
         {
             _rb.MovePosition(transform.position + (transform.right + transform.forward) * _speedPlayarMin * Time.fixedDeltaTime);
-            animator.SetBool("RightStrafeWalking", true);
-            animator.SetBool("Walcing", false);
+            _animator.SetBool("RightStrafeWalking", true);
+            _animator.SetBool("Walcing", false);
             AccelerationPlayar(0f);
         }
-
-
-
-        Vector3 movement = new Vector3(hInput, 0f, vInput);
-        _rb.velocity = movement * _speedPlayarMin * Time.deltaTime;
     }
+
+    // метод для выключения анимации 
     private void AnimationOff() 
     {
         if (Input.GetKeyUp(KeyCode.W))
         {
-            animator.SetBool("Walcing", false);
+            _animator.SetBool("Walcing", false);
             _speedPlayarMin = 0;
         }
 
         if (Input.GetKeyUp(KeyCode.S))
         {
-            animator.SetBool("WalcingBack", false);
+            _animator.SetBool("WalcingBack", false);
             _speedPlayarMin = 0;
         }
 
         if (Input.GetKeyUp(KeyCode.A))
         {
-            animator.SetBool("LeftForwardDiadonaleStrafeWalking", false);
+            _animator.SetBool("LeftForwardDiadonaleStrafeWalking", false);
             _speedPlayarMin = 0;
         }
-    }
-
-    private void FixedUpdate()
-    {
-        PlayerMovement();
-        AnimationOff();
     }
 }
